7c1f309b3d908de0ca646a9f0e1320f7ba6fc368
Rioa <rioaformyking@gmail.com>
2021-07-28 15:53:59 +0800

    Register function

--- src/main/java/com/Rioa/web/admin/LoginController.java
+++ src/main/java/com/Rioa/web/admin/LoginController.java
@@ -28,27 +28,6 @@ public class LoginController {
         return "admin/login";
     }
 
-    @PostMapping("/register")
-    public String register(HttpServletRequest request,
-                           RedirectAttributes attributes) {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        String confirmPassword = request.getParameter("confirmPassword");
-        if(password.equals(confirmPassword)) {
-            if(userService.checkUser(username,password) != null) {
-                attributes.addFlashAttribute("message", "该用户名已经存在");
-                return "redirect:/admin/login";
-            }else {
-                //add
-                User user = new User();
-                user.setUsername(username);
-                user.setPassword(password);
-                userRepository.save(user);
-                return "redirect:/admin/login";
-            }
-        }
-        return "admin/login";
-    }
 
     @PostMapping("/login")
     public String login(@RequestParam("username") String username,
